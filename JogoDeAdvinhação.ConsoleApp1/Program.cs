2474c6e269f7adb3167398a4ca2cebb1bd732515
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-12 14:56:05 -0300

    Add project files.

---
+++ JogoDeAdvinhação.ConsoleApp1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace JogoDeAdvinhação.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // loop principal 
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Jogo de Advinhação");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Em qual modo de dificuldade você gostaria de jogar?");
+                Console.WriteLine("1 - Fácil ");
+                Console.WriteLine("2 - Médio");
+                Console.WriteLine("3 - Difícil");
+
+                
+                string opcaoDificuldade = Console.ReadLine();
+                int TotalTentativas;
+                if (opcaoDificuldade == "1")
+                {
+                    Console.WriteLine("Ótimo, Você escolheu o Modo Fácil");
+                    TotalTentativas = 7;
+                    Console.WriteLine("Nesse modo, você terá 7 tentativas para acertar");
+                } else if (opcaoDificuldade == "2")
+                {
+                    Console.WriteLine("Ótimo, Você escolheu o Modo Médio");
+                    TotalTentativas = 5;
+                    Console.WriteLine("Nesse modo, você terá 5 tentativas para acertar");
+                }
+                else if (opcaoDificuldade == "3")
+                {
+                    Console.WriteLine("Ótimo, Você escolheu o Modo Difícil");
+                    TotalTentativas = 3;
+                    Console.WriteLine("Nesse modo, você terá 3 tentativas para acertar");
+                } else
+                {
+                    Console.WriteLine("Comando Inválido, Retornando ao Menu principal");
+                    continue;
+                }
+
+
+                    // gerar um número secreto aleatório
+                    Random geradorDeNumeros = new Random();
+                int NumeroSecreto = geradorDeNumeros.Next(1, 21);
+
+                //logica do jogo 
+                Console.WriteLine("---------------------------------");
+                for (int tentativas = 1; tentativas <= TotalTentativas; tentativas++ )
+              {
+                    
+                    Console.WriteLine($"Tentativa {tentativas} de {TotalTentativas} tentativas totais");
+                    Console.WriteLine("---------------------------------");
+
+                    Console.WriteLine("Digite um número entre 1 e 20: ");
+                    int NumeroDigitado = Convert.ToInt32(Console.ReadLine());
+                    while (NumeroDigitado > 20 || NumeroDigitado < 1)
+                    {
+                        Console.WriteLine("Número Inválido, digite novamente");
+                        NumeroDigitado = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                Console.WriteLine("Você digitou o número "+ NumeroDigitado);
+
+                if (NumeroDigitado == NumeroSecreto)
+                {
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("Parabéns, você ganhou o jogo!!");
+                    Console.WriteLine("---------------------------------");
+                        TotalTentativas = tentativas;
+                }
+
+                else if (NumeroDigitado < NumeroSecreto)
+                {
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("O numero digitado é menor que o numero secreto ");
+                    Console.WriteLine("---------------------------------");
+
+                }
+                else if (NumeroDigitado > NumeroSecreto)
+                {
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("O numero digitado é maior que o numero secreto");
+                    Console.WriteLine("---------------------------------");
+                }
+                    if(TotalTentativas == tentativas && NumeroSecreto != NumeroDigitado)
+                    {
+                        Console.WriteLine("Que Pena, você não conseguiu acertar o número secreto", "/n");
+                        Console.WriteLine($"O número secreto era {NumeroSecreto}", "/n");
+                    }
+              }
+
+                Console.WriteLine("Deseja continuar (s/n) ", "/n");
+                string opcaoContinuar = Console.ReadLine().ToUpper();               
+                if (opcaoContinuar != "S")
+                {
+                    Console.WriteLine("Obrigado Pela Presença");
+                    break;
+                } 
+
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# Jogo De Advinhação
+![](https://i.imgur.com/aZMWIGN.gif)
+Um jogo simples mas muito desafiador e emocionante
+
+## Funcionalidades
+
+**Número Aleatório** - O programa cria um número aleatório para o usuário descobir
+
+**Escolha de Dificuldade** - Permite ao Usuário escolher o nível de dificuldade 
+
+## Como Utilizar 
+
+1. Clone o repositório ou baixe o código fonte da Calculadora.
+2. Abra ou terminal ou o prompt de comando e navegue até a pasta raiz da Calculadora.
+3. Utilize o comando abaixo para restaurar as dependências do projeto.
+```
+dotnet restore
+```
+4. Em seguida, compile a solução utilizando o comando:
+```
+dotnet build --configuration Release
+```
+5. Para executar o projeto compilando em tempo real
+```
+dotnet run --project Calculadora.ConsoleApp
+```
+6. Para executar o arquivo compilado, navegue até a pasta ./JogoDeAdvinhação.ConsoleApp/bin/Release/net8.0/ e execute o arquivo:
+```
+JogoDeAdvinhação.ConsoleApp.exe
+```
+## Requisitos 
+
+.NET SDK (recomendado .NET 8.0 ou superior) para compilação e execução do projeto.
